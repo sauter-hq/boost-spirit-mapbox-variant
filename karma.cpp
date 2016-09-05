85076dfe9b07789caa00ab2558bd21d3ccfc469b
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2016-09-05 16:30:12 +0200

    BUGFIX: Working Karma integration of mapbox::variant.

--- karma.cpp
+++ karma.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 #include <mapbox/variant.hpp>
 
+typedef mapbox::util::variant<int, bool, std::string, float, long, uint64_t> frame;
+
 //namespace boost {
-//  using mapbox::util::get;
+//  template<class ResultType>
+//  inline ResultType get(frame const& x) {
+//    std::cout << "We are getting out of the vairant : " << typeid(ResultType).name() << std::endl;
+//    return mapbox::util::get<ResultType>(x);
+//  }
 //}
+
+namespace boost {
+  using mapbox::util::get;
+}
+
+
+
+
 
 #include <boost/spirit/include/karma.hpp>
 
-typedef mapbox::util::variant<int, bool, std::string> frame;
 
 template <class T, class... Types>
 struct is_in_type_list;
@@ -22,6 +35,7 @@ struct is_in_type_list<T, mapbox::util::variant<Types...>> {
   enum { value = type::value };
 
   enum { index = direct_type::index };
+  enum { size = sizeof...(Types) };
 };
 
 
@@ -34,24 +48,31 @@ namespace boost { namespace spirit { namespace traits
 }
 }}
 
-//
+namespace boost { namespace spirit { namespace traits {
+
+    template <class... Types>
+    struct variant_which< mapbox::util::variant<Types...> >
+    {
+        static int call(mapbox::util::variant<Types...> const& v)
+        {
+            auto idx = v.which(); //sizeof...(Types) - v.which() - 1;
+            std::cout << "Returning current type index " << idx << std::endl;
+            return idx;
+        }
+    };
+
+}}}
 
 namespace boost { namespace spirit { namespace traits {
 
-    ///////////////////////////////////////////////////////////////////////////
-    // The compute_compatible_component_variant for mapbox::util::variant
-    ///////////////////////////////////////////////////////////////////////////
-
-    namespace detail
-    {
-        BOOST_MPL_HAS_XXX_TRAIT_DEF(first_type)
-    }
-
-    template <typename Variant, typename Expected, typename Domain>
-    struct compute_compatible_component_varianta
+    template <typename Variant, typename Expected>
+    struct compute_compatible_component_variant<Variant, Expected, mpl::false_
+      , typename enable_if< is_same<Variant,frame> >::type>
     {
         typedef typename traits::variant_type<Variant>::type variant_type;
-        using type = is_in_type_list<Expected, Variant>;
+
+        // true_ if the attribute matches one of the types in the variant
+        typedef is_in_type_list<Expected, Variant> type;
         enum { value = type::value };
 
         // return the type in the variant the attribute is compatible with
@@ -62,130 +83,15 @@ namespace boost { namespace spirit { namespace traits {
         // return whether the given type is compatible with the Expected type
         static bool is_compatible(int which)
         {
-
-            std::cout << "OOK : " << v.which() << std::endl;
-            return which == type::index;
+          std::cout << "Size is :" << type::size << std::endl;
+          auto idx = (type::size - 1 - type::index); // Typelist is inverted and 0-based
+          std::cout << "We check if they are compatible " << idx << "-" << typeid(Expected).name() << " and " << which << std::endl;
+            return which == idx;
         }
     };
 
-    template <typename Expected, typename Attribute, typename Domain>
-    struct compute_compatible_component_a;
-
-    template <typename Expected,  typename Domain>
-    struct compute_compatible_component_a<Expected, frame, Domain>
-      : compute_compatible_component_varianta<frame, Expected, Domain> {};
-
-
-    ///////////////////////////////////////////////////////////////////////////
-    // return the type currently stored in the given variant
-    template <class... Types>
-    struct variant_which<mapbox::util::variant<Types...> >
-    {
-        static int call(mapbox::util::variant<Types...> const& v)
-        {
-            std::cout << "OOK : " << v.which() << std::endl;
-            return v.which();
-        }
-    };
 
 }}}
-
-//
-//
-//
-//
-
-namespace boost { namespace spirit { namespace karma { namespace detail
-{
-
-
-   template <typename Component, typename Expected>
-    struct alternative_generate<Component, frame, Expected
-      , typename std::enable_if< not traits::compute_compatible_component_varianta<frame, Expected, domain>::value > >
-            /*traits::compute_compatible_component<Expected, Attribute, karma::domain> >::type*/
-    {
-
-        typedef frame Attribute;
-
-        template <typename OutputIterator, typename Context, typename Delimiter>
-        static bool
-        call(Component const& component, OutputIterator& sink
-          , Context& ctx, Delimiter const& d, Attribute const& attr, bool&)
-        {
-          std::cout << "oops" << std::endl;
-          return false;
-        }
-
-    };
-
-
-    //  this gets instantiated if the Attribute type is compatible to the
-    //  generator
-    template <typename Component, typename Expected>
-    struct alternative_generate<Component, frame, Expected
-      , typename enable_if< typename traits::compute_compatible_component_varianta<frame, Expected, domain>::type >::type*
-
-      //, typename enable_if< is_same<frame, Attribute> >::type
-            /*traits::compute_compatible_component<Expected, Attribute, karma::domain> >::type*/>
-    {
-        typedef frame Attribute;
-
-        template <typename OutputIterator, typename Context, typename Delimiter>
-        static bool
-        call(Component const& component, OutputIterator& sink
-          , Context& ctx, Delimiter const& d, Attribute const& attr, bool&)
-        {
-          std::cout << "Got called in specialization !" << std::endl;
-            return call(component, sink, ctx, d, attr
-              , spirit::traits::not_is_variant<Attribute, karma::domain>());
-        }
-
-        template <typename OutputIterator, typename Context, typename Delimiter>
-        static bool
-        call(Component const& component, OutputIterator& sink
-          , Context& ctx, Delimiter const& d, Attribute const& attr, mpl::true_)
-        {
-          std::cout << "Got called in specialization 1 !" << std::endl;
-            return component.generate(sink, ctx, d, attr);
-        }
-
-        template <typename OutputIterator, typename Context, typename Delimiter>
-        static bool
-        call(Component const& component, OutputIterator& sink
-          , Context& ctx, Delimiter const& d, Attribute const& attr, mpl::false_)
-        {
-          std::cout << "Got called in specialization 2 !" << std::endl;
-            typedef
-                traits::compute_compatible_component_varianta<Attribute, Expected, domain>
-            component_type;
-
-            // if we got passed an empty optional, just fail generation
-            if (!traits::has_optional_value(attr))
-                return false;
-
-            // make sure, the content of the passed variant matches our
-            // expectations
-            typename traits::optional_attribute<Attribute>::type attr_ = 
-                traits::optional_value(attr);
-            if (!component_type::is_compatible(spirit::traits::which(attr_)))
-                return false;
-
-            // returns true if any of the generators succeed
-            typedef typename component_type::compatible_type compatible_type;
-            return component.generate(sink, ctx, d
-              , mapbox::util::get<compatible_type>(attr_));
-        }
-
-    };
-
-
-}}}}
-
-
-
-
-
-
 
 
 struct visitor {
@@ -202,10 +108,12 @@ int main(int argc, char** argv) {
 
   using namespace boost::spirit::karma;
 
+  rule<Iterator, std::string()> mystring = *char_;
+
   rule<Iterator, frame()> r = 
       int_
     | bool_
-    | *char_
+    | mystring
   ;
 
   {
@@ -227,7 +135,7 @@ int main(int argc, char** argv) {
   }
 
   {
-    frame frm = std::string{"Ma grosse banana"};
+    frame frm = std::string{"this is a string"};
     std::string buf;
     Iterator iter(buf);
     auto s = generate(iter, r, frm); 
